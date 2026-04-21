2a0d283dfcde2d33e2f30dfc95dd8bc2c3059b7a
Rishi A Sheth <rishisheth404@gmail.com>
2026-04-21 11:28:23 +0530

    Create PatientRepository interface for patient data

--- src/main/java/com/appointment/repository/PatientRepository.java
+++ src/main/java/com/appointment/repository/PatientRepository.java
@@ -1,1 +1,12 @@
-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+package com.appointment.repository;
+
+import com.appointment.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Optional<Patient> findByEmail(String email);
+}
